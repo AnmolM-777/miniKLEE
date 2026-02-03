5ce4c7d93f442eafab944d306c6c3fcb40307842
Anmol Mishra <anmolmishra777@gmail.com>
2026-02-03 11:57:40 +0530

    docs(readme): clarify Z3 theorem prover linkage [step 5]

--- include/z3_solver.h
+++ include/z3_solver.h
@@ -5,3 +5,5 @@
 #include <map>
 class Z3SolverWrapper { std::map<std::string, int> symbolic_vars; std::vector<std::string> constraints; public: void add_symbolic_var(const std::string& name, int bit_width); void add_path_constraint(const std::string& constraint); bool is_satisfiable(); std::string get_model_assignment(const std::string& var); };
 #endif
+
+// Incremental development step #5
