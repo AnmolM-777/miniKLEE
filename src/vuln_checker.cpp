e5dee24d027e6f2a4893cbe1c2e5bd5a8d7c9816
Anmol Mishra <anmolmishra777@gmail.com>
2026-02-10 19:30:20 +0530

    refactor(solver): cache SAT query solver contexts [step 20]

--- src/vuln_checker.cpp
+++ src/vuln_checker.cpp
@@ -3,3 +3,5 @@ bool VulnChecker::check_cwe121_stack_overflow(Z3SolverWrapper& solver, int buffe
 bool VulnChecker::check_cwe122_heap_overflow(Z3SolverWrapper& solver, int alloc_size) { solver.add_path_constraint("heap_offset >= " + std::to_string(alloc_size)); return solver.is_satisfiable(); }
 
 // Incremental development step #8
+
+// Incremental development step #20
