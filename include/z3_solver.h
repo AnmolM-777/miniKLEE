bc5fe8e3a03a801b3318a69e16653a3e1b448699
Anmol Mishra <anmolmishra777@gmail.com>
2026-03-04 17:55:17 +0530

    refactor(solver): cache SAT query solver contexts [step 65]

--- include/z3_solver.h
+++ include/z3_solver.h
@@ -15,3 +15,5 @@ class Z3SolverWrapper { std::map<std::string, int> symbolic_vars; std::vector<st
 // Incremental development step #41
 
 // Incremental development step #53
+
+// Incremental development step #65
