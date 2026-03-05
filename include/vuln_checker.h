70d330fc42f61b8d7542aab9f4c55269043d0bc5
Anmol Mishra <anmolmishra777@gmail.com>
2026-03-05 17:49:52 +0530

    test(cwe121): add nested loop stack overflow test case [step 67]

--- include/vuln_checker.h
+++ include/vuln_checker.h
@@ -14,3 +14,5 @@ class VulnChecker { public: static bool check_cwe121_stack_overflow(Z3SolverWrap
 // Incremental development step #43
 
 // Incremental development step #55
+
+// Incremental development step #67
